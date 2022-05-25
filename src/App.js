fe8d90c191f4bb175a612bcf0833d77be1caff9b
Emilien1996 <emil.mods@mail.ru>
2022-05-26 01:32:25 +0400

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./layout/Header";
+import Contact from "./pages/Contact/idex";
 import { ProjectPage } from "./pages/Project";
+import About from "./pages/About";
+import NotFound from "./pages/404 page";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ProjectPage />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProjectPage />}></Route>
+          <Route path="/About" element={<About />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/layout/Navbar/index.jsx
+++ src/layout/Navbar/index.jsx
@@ -1,39 +1,20 @@
-import { useState } from "react";
-import { NAVBAR_LINKS } from "../../consts";
-import { NavItem } from "./NavItem";
+import { Link } from "react-router-dom";
+
 import "./styles.css";
 
-const { project, contact, aboutMe } = NAVBAR_LINKS;
-
 export const Navbar = () => {
-  const [activeLink, setActiveLink] = useState(project.link);
-
-  const navItemClickHandler = (link) => {
-    setActiveLink(link);
-  };
-
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <NavItem
-          label={aboutMe.label}
-          link={aboutMe.link}
-          isActive={activeLink === aboutMe.link}
-          onClick={navItemClickHandler}
-          // onClick={()=>navItemClickHandler(aboutMe.link)}
-        />
-        <NavItem
-          label={project.label}
-          link={project.link}
-          isActive={activeLink === project.link}
-          onClick={navItemClickHandler}
-        />
-        <NavItem
-          label={contact.label}
-          link={contact.link}
-          isActive={activeLink === contact.link}
-          onClick={navItemClickHandler}
-        />
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+        <li>
+          <Link to="/">Project</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
